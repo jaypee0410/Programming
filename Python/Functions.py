303256c0d7a5cb01ddc977dabe71d8564f88a053
John Philip <jpcabalitan24@gmail.com>
2023-09-25 15:14:59 +0800

    Modified files

---
+++ Python/Functions.py
@@ -0,0 +1,6 @@
+def my_func(x, y):
+    add = x+y
+    return add
+
+print(my_func(10, 2))
+
---
+++ Python/string-formatting2.py
@@ -0,0 +1,12 @@
+#nums = [4, 5, 6]
+#msg = "Numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2])
+#print(msg)
+
+#print("{0}{1}{0}".format("abra", "cad"))
+
+#a = "{x}, {y}".format(x=5, y=12)
+#print(a)
+
+x = ", ".join(["spam", "eggs", "ham"])
+print((x))
+##prints "spam, eggs, ham"
