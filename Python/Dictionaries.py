f61b458216dd550168879ab97736b553a041b315
John Philip <jpcabalitan24@gmail.com>
2023-09-26 17:43:08 +0800

    Modified files

---
+++ Python/Dictionaries.py
@@ -0,0 +1,7 @@
+ages = {
+   "Dave": 24,
+   "Mary": 42,
+   "John": 58
+}
+print(ages["Dave"])
+print(ages["Mary"])
--- Python/Functions.py
+++ Python/Functions.py
@@ -1,6 +1,19 @@
+"""
 def my_func(x, y):
     add = x+y
     return add
 
 print(my_func(10, 2))
+"""
+def print_nums(x):
+  for i in range(x):
+    print(i)
+    return
+print_nums(10)
 
+def func(x):
+  res = 0
+  for i in range(x):
+     res += i
+  return res
+print(func(3))
