c6b287a6a54c1b97ebc1c09d4665299e71365882
John Philip <jpcabalitan24@gmail.com>
2024-06-17 11:33:21 +0800

    modified files

---
+++ C++/input_output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    int i;
+    std::cout << "Enter a value:" << std::endl;
+    std::cin >> i;
+    std::cout << "You entered: " << i << std::endl;
+
+    return 0;
+}
